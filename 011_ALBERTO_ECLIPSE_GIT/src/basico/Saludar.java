3f410ba4856c3132b81a3842fcbc17464fda1d17
Curso mañana <Curso maana@PC05a5>
2018-01-29 10:34:21 +0100

    subnormal

--- 011_ALBERTO_ECLIPSE_GIT/src/basico/Saludar.java
+++ 011_ALBERTO_ECLIPSE_GIT/src/basico/Saludar.java
@@ -3,7 +3,8 @@ package basico;
 public class Saludar {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola soy Alberto");
+		System.out.println("Hola soy Alberto, y me enfado con mis amigos "
+				+ "porque no me llevan de pokeincursion xD");
 	}
 
 }
